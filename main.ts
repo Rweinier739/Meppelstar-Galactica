961448bc34dbea89085f91fffda3be47676e24ea
Rweinier739 <100585446+Rweinier739@users.noreply.github.com>
2022-04-11 14:24:14 +0200

    levens laten zien

--- main.ts
+++ main.ts
@@ -6,7 +6,33 @@ radio.onReceivedNumber(function (receivedNumber) {
     }
     if (vijandkogel.isTouching(ruimteschip)) {
         levens += -1
+        music.ringTone(131)
         vijandkogel.delete()
+        if (levens == 2) {
+            leven1 = game.createSprite(1, 0)
+            leven2 = game.createSprite(2, 0)
+            for (let index = 0; index < 4; index++) {
+                leven3 = game.createSprite(3, 0)
+                basic.pause(200)
+                leven3.delete()
+                basic.pause(200)
+            }
+            leven1.delete()
+            leven2.delete()
+        }
+        if (levens == 1) {
+            leven1 = game.createSprite(1, 0)
+            for (let index = 0; index < 4; index++) {
+                leven2 = game.createSprite(2, 0)
+                basic.pause(200)
+                leven2.delete()
+                basic.pause(200)
+            }
+            leven1.delete()
+        }
+        if (levens == 0) {
+            game.gameOver()
+        }
     } else {
         basic.pause(500)
         vijandkogel.delete()
@@ -30,12 +56,18 @@ input.onButtonPressed(Button.B, function () {
 })
 let jouwkogel: game.LedSprite = null
 let vijandkogel: game.LedSprite = null
+let leven3: game.LedSprite = null
+let leven2: game.LedSprite = null
+let leven1: game.LedSprite = null
+let levens = 0
 let ruimteschip: game.LedSprite = null
 radio.setGroup(33)
 ruimteschip = game.createSprite(2, 4)
-let levens = 3
-basic.forever(function () {
-    if (levens == 0) {
-        game.gameOver()
-    }
-})
+levens = 3
+leven1 = game.createSprite(1, 0)
+leven2 = game.createSprite(2, 0)
+leven3 = game.createSprite(3, 0)
+basic.pause(1000)
+leven1.delete()
+leven2.delete()
+leven3.delete()
